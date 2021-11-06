1d53cf52915158f76337311cd48373be8250a01e
nacanz <naca.anggitian22@gmail.com>
2021-11-06 21:29:05 +0700

    add router progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress
+        height={5}
+        color="#4338c9"
+        options={{
+          showSpinner: false,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
